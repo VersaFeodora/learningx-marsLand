037891cdeb8768ec86b45d6d74204f461e2f5f1a
VersaFeodora <2141720156@student.polinema.ac.id>
2023-01-06 16:20:54 +0700

    Conf

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("mongodb+srv://test:sparta@cluster0.btcdwe9.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME =  os.environ.get("dbsparta")
+
 client = MongoClient('mongodb+srv://test:sparta@cluster0.btcdwe9.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
